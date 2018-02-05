34fefaf48f4731d098c30330cb076fa7bb919c75
Unknown <dmitriy.semashkov@makingwaves.com>
2018-02-05 12:03:56 +0100

    rework mouth detection
    - add Mouth class to keep statistics
    - avoid chatter by aggregating conequent signals  
    - use maximum mouth size instead of minimum
    - add verbose and visual modes

--- FlapPyBird/mouthdetector.py
+++ FlapPyBird/mouthdetector.py
@@ -4,8 +4,10 @@ import imutils
 import numpy as np
 import queue
 import dlib
+import collections
 from facialcalc import distanceBetweenMouth, heightFace
-
+from imutils.video import VideoStream
+import time
 
 def land2coords(landmarks, dtype="int"):
     # initialize the list of tuples
@@ -20,32 +22,89 @@ def land2coords(landmarks, dtype="int"):
     # return the list of (a, b)-coordinates
     return coords
 
+class Mouth:
+    def __init__(self):
+        self.min_size = collections.deque(maxlen=3)
+        self.max_size = collections.deque(maxlen=3)
+    
+    def empty(self):
+        return len(self.min_size)==0
+
+    def get_min(self):
+        return np.mean(self.min_size) if not self.empty() else 99999
+
+    def get_max(self):
+        return np.mean(self.max_size) if not self.empty() else 0
+
+    def append(self, size, delta = 0):
+        size_low = size + delta
+        size_high = size - delta
+        
+        if self.empty():
+            self.min_size.append(size_low)
+            self.max_size.append(size_high)
+            return
+        
+        mn,mx = self.get_min(), self.get_max()
+        avg = 0.5*(mx+mn)
+
+        if size_low < mn:
+            self.min_size.append(size_low)
+        
+        if size_high > mx:
+            self.max_size.append(size_high)
+        
+
+    def __str__(self):
+        return "min: {}, max: {}".format(np.round(self.get_min(),2), np.round(self.get_max(),2) )
+
 class MouthDetector:
-
-
 
     def __init__(self, detectorfile):
         self.face_detector = dlib.get_frontal_face_detector()
         self.landmark_predictor = dlib.shape_predictor(detectorfile)
-        self.vid = cv2.VideoCapture(0)
-        self.minimumDistances = {}
-        self.is_open = False
-        self.events = queue.Queue()
-        self.id = 0
+        #self.vid = cv2.VideoCapture(0)
+        self.vid = VideoStream(usePiCamera=False).start()
+        self.mouth_points = np.arange(48,68)
+        self.show = False
+        self.verbose = False
+
+        self.reset()
+
+    def reset(self):
+        self.aspects = {}
+        self.last_state = None
+        self.tmp_state = None
+        self.tmp_state_count = 0
+        self.last_time = time.perf_counter()
+
 
     def get_last(self):
         try:
             return self.events.get(False)
         except:
-            return False
+            return None
 
+    def toggle_display(self):
+        self.show = not self.show
+
+    def toggle_verbose(self):
+        self.verbose = not self.verbose
+
+    def detect_timed(self, resolution=0.04):
+        now = time.perf_counter()
+        if now - self.last_time > resolution:
+            self.last_time = now
+            return self.detect()
+        return None
+    
     def detect(self):
-        self.id = self.id + 1
-        _, frame = self.vid.read()
+        frame = self.vid.read()
 
         # resizing frame
         # you can use cv2.resize but I recommend imutils because its easy to use
         frame = imutils.resize(frame, width=400)
+        # frame = cv2.resize(frame, (400, int(400.0*h/w)), interpolation = cv2.INTER_CUBIC)
 
         # grayscale conversion of image because it is computationally efficient
         # to perform operations on single channeled (grayscale) image
@@ -59,28 +118,72 @@ class MouthDetector:
             # converting co-ordinates to NumPy array
             landmarks = land2coords(landmarks)
 
-            key = heightFace(landmarks) // 2
-            if key in self.minimumDistances:
-                minimumMouthDistance = self.minimumDistances[key]
-            elif key-1 in self.minimumDistances:
-                minimumMouthDistance = self.minimumDistances[key-1]
-            elif key+1 in self.minimumDistances:
-                minimumMouthDistance = self.minimumDistances[key+1]
-            else:
-                minimumMouthDistance = 99999
+            distanceMouth = np.round(distanceBetweenMouth(landmarks), 3)
 
-            distanceMouth = distanceBetweenMouth(landmarks)
-            if distanceMouth < minimumMouthDistance:
-                self.minimumDistances[key] = distanceMouth
+            height = heightFace(landmarks)
+            key = height//5
 
-            is_open_now = distanceMouth > 1.30 * minimumMouthDistance
+            for k in range(100):
+                m = self.aspects.setdefault(k, Mouth())
+                m.append(distanceMouth, max(0, k - key )*2)
 
-            print(is_open_now)
+            mouth = self.aspects[key]
+            open_ratio = np.round(distanceMouth / mouth.get_max(),2) if distanceMouth >=3 else 0
 
-            if is_open_now != self.is_open:
-                self.events.put(is_open_now)
+            old_state = self.last_state
+            is_open = self.update_state( open_ratio )
 
-            self.is_open = is_open_now
+            if self.verbose:
+                if is_open:
+                    symbol = '[O]' if self.last_state else '[o]'
+                else:
+                    symbol = '[.]' if self.last_state else '[-]'
+
+                print( symbol , "ratio", open_ratio, mouth, "a", key )
+
+            if self.show:
+                
+                color = (0, 0, 255) if self.last_state else (255,0,0)
+                
+                for i in self.mouth_points:
+                    cv2.circle(frame, tuple(landmarks[i]), 2, color, -1)
+
+                cv2.imshow("frame", frame)
+
+            if old_state == self.last_state:
+                return None
+            
+            return self.last_state
+
+    def update_state(self, open_ratio):
+        
+        state = open_ratio >= 0.4
+
+        if open_ratio >= 0.7 or open_ratio <= 0.1:
+            repetitions = 0
+        elif open_ratio >= 0.6 or open_ratio <= 0.2:
+            repetitions = 1
+        else:
+            repetitions = 3
+
+        if repetitions == 0:
+            self.last_state = state
+        elif self.tmp_state != state:
+            self.tmp_state = state
+            self.tmp_state_count = 1
+        elif self.tmp_state_count >= repetitions:
+            self.tmp_state = None
+            self.tmp_state_count = 0
+            self.last_state = state
+        else:
+            self.tmp_state_count += 1 
+
+        return state
+
+            
+
+
+            
 
 
 
